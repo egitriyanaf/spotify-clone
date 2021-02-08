cae20fe33c6029885ec34d7bf0a6724f2cabcb8c
egitriyanaf <egitriyanafidollah@gmail.com>
2021-02-08 18:42:21 +0700

    make spotify button and adding logo spotify

---
+++ src/spotify.js
@@ -0,0 +1,11 @@
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "23e4f5b3202444bdb2df2e8de214726f";
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
